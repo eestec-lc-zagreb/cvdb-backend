a17e764d6e49bda26469c063b2d0619ddfb7a3e4
Alen Hrga <alen.hrga@ingemark.com>
2017-07-13 12:40:08 +0200

    Initial project setup

---
+++ src/main/java/hr/eestec_zg/cvdbbackend/CvdbBackendApplication.java
@@ -0,0 +1,12 @@
+package hr.eestec_zg.cvdbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvdbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvdbBackendApplication.class, args);
+	}
+}
